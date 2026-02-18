72f208a5fd3f4f6530248efc7fd29eed54926830
SifanGeremu <wins33975@gmail.com>
2026-02-18 11:17:33 +0300

    feat(auth): add LogoutController with Sanctum support

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,10 +5,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\GetAuthenticatedUserController;
+use App\Http\Controllers\LogoutController;
+//protected routes
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', GetAuthenticatedUserController::class);
+    Route::post('/logout', LogoutController::class);
+});
 
 Route::post('/register',RegisterController::class);
 Route::post('/login',LoginController::class);
