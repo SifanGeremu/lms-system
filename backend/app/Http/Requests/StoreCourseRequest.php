9f7c3502fe0b238bf2b0eb3e65d90e9713dd7b8e
SifanGeremu <wins33975@gmail.com>
2026-02-21 10:29:26 +0300

    feat(request)- Add storeCourseRequest

---
+++ backend/app/Http/Requests/StoreCourseRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        'title' => 'required|string|max:255',
+        'slug' => 'required|string|unique:courses,slug|max:255',
+        'description' => 'nullable|string',
+        'category' => 'required|exists:categories,id',
+        'status' => 'required|in:draft,published',
+        'published_at' => 'nullable|date',
+        'thumbnail' => 'nullable|image|max:2048',
+
+        ];
+    }
+}
