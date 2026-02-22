bd6d45c59fa88a93a14384a3504f088f40e97ca9
SifanGeremu <wins33975@gmail.com>
2026-02-22 17:09:48 +0300

    Update DatabaseSeeder with course/category data

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-       $this->call(RoleSeeder::class);
+        $this->call([
+            RoleSeeder::class,
+            CategorySeeder::class,
+            CourseSeeder::class,
+        ]);
     }
 }
